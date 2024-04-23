a76f6e7d9b147adba43b3eb1f5109b0de36c2dbf
Yu SuiXian <47711102+GengGode@users.noreply.github.com>
2024-04-23 17:47:47 +0800

    Refactor: 调整节点声明函数位置

--- blueprints-example/nodes/base_nodes.cpp
+++ blueprints-example/nodes/base_nodes.cpp
@@ -1,4 +1,5 @@
 #include "base_nodes.hpp"
+#include "child_nodes/child_nodes.hpp"
 #include "../notifiers/Notifier.hpp"
 #include <filesystem>
 
@@ -32,106 +33,6 @@ ExecuteResult get_value(Graph *graph, Pin input, T &value)
     return ExecuteResult::Success();
 }
 
-Node *SpawnInputActionNode(const std::function<int()> &GetNextId, const std::function<void(Node *)> &BuildNode, std::vector<Node> &m_Nodes)
-{
-    m_Nodes.emplace_back(GetNextId(), "InputAction Fire", ImColor(255, 128, 128));
-    m_Nodes.back().Outputs.emplace_back(GetNextId(), "", PinType::Delegate);
-    m_Nodes.back().Outputs.emplace_back(GetNextId(), "Pressed", PinType::Flow);
-    m_Nodes.back().Outputs.emplace_back(GetNextId(), "Released", PinType::Flow);
-
-    BuildNode(&m_Nodes.back());
-
-    return &m_Nodes.back();
-}
-
-Node *SpawnBranchNode(const std::function<int()> &GetNextId, const std::function<void(Node *)> &BuildNode, std::vector<Node> &m_Nodes)
-{
-    m_Nodes.emplace_back(GetNextId(), "Branch");
-    m_Nodes.back().Inputs.emplace_back(GetNextId(), "", PinType::Flow);
-    m_Nodes.back().Inputs.emplace_back(GetNextId(), "Condition", PinType::Bool);
-    m_Nodes.back().Outputs.emplace_back(GetNextId(), "True", PinType::Flow);
-    m_Nodes.back().Outputs.emplace_back(GetNextId(), "False", PinType::Flow);
-
-    BuildNode(&m_Nodes.back());
-
-    return &m_Nodes.back();
-}
-
-Node *SpawnDoNNode(const std::function<int()> &GetNextId, const std::function<void(Node *)> &BuildNode, std::vector<Node> &m_Nodes)
-{
-    m_Nodes.emplace_back(GetNextId(), "Do N");
-    m_Nodes.back().Inputs.emplace_back(GetNextId(), "Enter", PinType::Flow);
-    m_Nodes.back().Inputs.emplace_back(GetNextId(), "N", PinType::Int);
-    m_Nodes.back().Inputs.emplace_back(GetNextId(), "Reset", PinType::Flow);
-    m_Nodes.back().Outputs.emplace_back(GetNextId(), "Exit", PinType::Flow);
-    m_Nodes.back().Outputs.emplace_back(GetNextId(), "Counter", PinType::Int);
-
-    BuildNode(&m_Nodes.back());
-
-    return &m_Nodes.back();
-}
-
-Node *SpawnOutputActionNode(const std::function<int()> &GetNextId, const std::function<void(Node *)> &BuildNode, std::vector<Node> &m_Nodes)
-{
-    m_Nodes.emplace_back(GetNextId(), "OutputAction");
-    m_Nodes.back().Inputs.emplace_back(GetNextId(), "Sample", PinType::Float);
-    m_Nodes.back().Outputs.emplace_back(GetNextId(), "Condition", PinType::Bool);
-    m_Nodes.back().Inputs.emplace_back(GetNextId(), "Event", PinType::Delegate);
-
-    BuildNode(&m_Nodes.back());
-
-    return &m_Nodes.back();
-}
-
-Node *SpawnPrintStringNode(const std::function<int()> &GetNextId, const std::function<void(Node *)> &BuildNode, std::vector<Node> &m_Nodes)
-{
-    m_Nodes.emplace_back(GetNextId(), "Print String");
-    m_Nodes.back().Inputs.emplace_back(GetNextId(), "", PinType::Flow);
-    m_Nodes.back().Inputs.emplace_back(GetNextId(), "In String", PinType::String);
-    m_Nodes.back().Outputs.emplace_back(GetNextId(), "", PinType::Flow);
-
-    BuildNode(&m_Nodes.back());
-
-    return &m_Nodes.back();
-}
-
-Node *SpawnMessageNode(const std::function<int()> &GetNextId, const std::function<void(Node *)> &BuildNode, std::vector<Node> &m_Nodes)
-{
-    m_Nodes.emplace_back(GetNextId(), "", ImColor(128, 195, 248));
-    auto &node = m_Nodes.back();
-    node.Type = NodeType::Simple;
-    node.Outputs.emplace_back(GetNextId(), "Message", PinType::String, std::string("Hello, World!"));
-
-    node.OnExecute = [](Graph *graph, Node *node)
-    {
-        std::string message;
-        auto result = get_value(graph, node->Outputs[0], message);
-        if (result.has_error())
-            return result;
-        printf("Message: %s\n", message.c_str());
-        return ExecuteResult::Success();
-    };
-
-    BuildNode(&node);
-
-    return &node;
-}
-
-Node *SpawnSetTimerNode(const std::function<int()> &GetNextId, const std::function<void(Node *)> &BuildNode, std::vector<Node> &m_Nodes)
-{
-    m_Nodes.emplace_back(GetNextId(), "Set Timer", ImColor(128, 195, 248));
-    m_Nodes.back().Inputs.emplace_back(GetNextId(), "", PinType::Flow);
-    m_Nodes.back().Inputs.emplace_back(GetNextId(), "Object", PinType::Object);
-    m_Nodes.back().Inputs.emplace_back(GetNextId(), "Function Name", PinType::Function);
-    m_Nodes.back().Inputs.emplace_back(GetNextId(), "Time", PinType::Float);
-    m_Nodes.back().Inputs.emplace_back(GetNextId(), "Looping", PinType::Bool);
-    m_Nodes.back().Outputs.emplace_back(GetNextId(), "", PinType::Flow);
-
-    BuildNode(&m_Nodes.back());
-
-    return &m_Nodes.back();
-}
-
 Node *SpawnIntToStringNode(const std::function<int()> &GetNextId, const std::function<void(Node *)> &BuildNode, std::vector<Node> &m_Nodes)
 {
     m_Nodes.emplace_back(GetNextId(), "", ImColor(128, 195, 248));
@@ -153,61 +54,6 @@ Node *SpawnIntToStringNode(const std::function<int()> &GetNextId, const std::fun
     BuildNode(&node);
 
     return &node;
-}
-
-Node *SpawnLessNode(const std::function<int()> &GetNextId, const std::function<void(Node *)> &BuildNode, std::vector<Node> &m_Nodes)
-{
-    m_Nodes.emplace_back(GetNextId(), "<", ImColor(128, 195, 248));
-    m_Nodes.back().Type = NodeType::Simple;
-    m_Nodes.back().Inputs.emplace_back(GetNextId(), "", PinType::Float);
-    m_Nodes.back().Inputs.emplace_back(GetNextId(), "", PinType::Float);
-    m_Nodes.back().Outputs.emplace_back(GetNextId(), "", PinType::Float);
-
-    BuildNode(&m_Nodes.back());
-
-    return &m_Nodes.back();
-}
-
-Node *SpawnWeirdNode(const std::function<int()> &GetNextId, const std::function<void(Node *)> &BuildNode, std::vector<Node> &m_Nodes)
-{
-    m_Nodes.emplace_back(GetNextId(), "o.O", ImColor(128, 195, 248));
-    m_Nodes.back().Type = NodeType::Simple;
-    m_Nodes.back().Inputs.emplace_back(GetNextId(), "", PinType::Float);
-    m_Nodes.back().Outputs.emplace_back(GetNextId(), "", PinType::Float);
-    m_Nodes.back().Outputs.emplace_back(GetNextId(), "", PinType::Float);
-
-    BuildNode(&m_Nodes.back());
-
-    return &m_Nodes.back();
-}
-
-Node *SpawnTraceByChannelNode(const std::function<int()> &GetNextId, const std::function<void(Node *)> &BuildNode, std::vector<Node> &m_Nodes)
-{
-    m_Nodes.emplace_back(GetNextId(), "Single Line Trace by Channel", ImColor(255, 128, 64));
-    m_Nodes.back().Inputs.emplace_back(GetNextId(), "", PinType::Flow);
-    m_Nodes.back().Inputs.emplace_back(GetNextId(), "Start", PinType::Flow);
-    m_Nodes.back().Inputs.emplace_back(GetNextId(), "End", PinType::Int);
-    m_Nodes.back().Inputs.emplace_back(GetNextId(), "Trace Channel", PinType::Float);
-    m_Nodes.back().Inputs.emplace_back(GetNextId(), "Trace Complex", PinType::Bool);
-    m_Nodes.back().Inputs.emplace_back(GetNextId(), "Actors to Ignore", PinType::Int);
-    m_Nodes.back().Inputs.emplace_back(GetNextId(), "Draw Debug Type", PinType::Bool);
-    m_Nodes.back().Inputs.emplace_back(GetNextId(), "Ignore Self", PinType::Bool);
-    m_Nodes.back().Outputs.emplace_back(GetNextId(), "", PinType::Flow);
-    m_Nodes.back().Outputs.emplace_back(GetNextId(), "Out Hit", PinType::Float);
-    m_Nodes.back().Outputs.emplace_back(GetNextId(), "Return Value", PinType::Bool);
-
-    BuildNode(&m_Nodes.back());
-
-    return &m_Nodes.back();
-}
-
-Node *SpawnComment(const std::function<int()> &GetNextId, const std::function<void(Node *)> &BuildNode, std::vector<Node> &m_Nodes)
-{
-    m_Nodes.emplace_back(GetNextId(), "Test Comment");
-    m_Nodes.back().Type = NodeType::Comment;
-    m_Nodes.back().Size = ImVec2(300, 200);
-
-    return &m_Nodes.back();
 }
 
 Node *Spawn_ImageSource(const std::function<int()> &GetNextId, const std::function<void(Node *)> &BuildNode, std::vector<Node> &m_Nodes)
